bf57092cd844586c0846aac132388ce9a924433a
Marcus Hornby <marcushornby@outlook.com>
2019-02-17 20:05:09 +0000

    adding the final touches

--- README.md
+++ README.md
@@ -1,26 +1,52 @@
-     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
-    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
-    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
-    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
-     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
-    ----------------------------------------------------------------- 
+Write a README.md file for your project that explains what the project does and the need that it fulfills.
+It should also describe the functionality of the project, as well as the technologies used.
+Detail how the project was deployed and tested and if some of the work was based on other code, 
+explain what was kept and how it was changed to fit your need. 
+A project submitted without a README.md file will FAIL.
+
+Marcus Hornby Memory Game
+
+This is my 2nd Milestone project while studying under the Code Institute.
+
+The brief of the project was to create a responsive game based on the simon memory game.
+
+The basic structure and style was completed using html5 and css3 and the functionality was created using Jquery functions and events.
+
+I was unsure how to generate a random pattern and originally wrote code for each individual sequence which would always stay the same on each loading of the webpage.
+
+I spoke with my Mentor and he offered me some advice and told me to do some research
+
+to find the function that allowed me to generate things randomly '(Math.random() * num) + 1' or '[Math.floor(Math.random()*4)];'
 
 
-Hi there! Welcome to Cloud9 IDE!
-
-To get you started, we included a small hello world application.
-
-1) Open the hello-world.html file
-
-2) Click on the Preview button to open a live preview pane
-
-3) Make some changes to the file, save, watch the preview, and have fun!
-
-Happy coding!
-The Cloud9 IDE team
+I researched how to create a random pattern which added a color:
 
 
-## Support & Documentation
+function getColor(num) {
+  /* Generate a random number between 1 and 4
+  Green: 1   Red: 2   Blue: 3   Yellow: 4
+  */
+  return Math.floor(Math.random() * num) + 1;
+}
 
-Visit http://docs.c9.io for documentation, or http://support.c9.io for support.
-To watch some training videos, visit http://www.youtube.com/user/c9ide
+and  then to input a sequence:
+
+function getSequence(){
+    let buttons = ["red","green","yellow","blue"];
+    let retArr = [];
+    while(retArr.length<20){
+        let button = buttons[Math.floor(Math.random()*4)];
+        retArr.push(button);
+    }
+
+    return retArr;
+}
+
+I also found out how to add sound effects to my game via the two github accounts above 
+and used a free website to add their sounds to my : https://www.soundjay.com/
+
+The testing was completed by adding incorrect code and running the app to see how it functioned
+and then considering ways that the code may fail if different options were selected for running the function.
+
+The way that I would improve this further is to implement a scoring system and a number of
+attempts system which I would load and store users and their score using JSON
--- js/script.js
+++ js/script.js
@@ -1,19 +1,140 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
+// Game Vars
+var game = {
+  count: 0,
+  colors: ["#green", "#red", "#yellow", "#blue"],
+  completePattern: [],
+  randomisePattern: [],
+  sound: {
+    green: new Audio('https://www.soundjay.com/button/sounds/beep-07.mp3'),
+    red: new Audio('https://www.soundjay.com/button/sounds/beep-08b.mp3'),
+    blue: new Audio('https://www.soundjay.com/button/sounds/beep-10.mp3'),
+    yellow: new Audio('https://www.soundjay.com/button/sounds/beep-03.mp3')
+  },
+  difficult: false,
+
+}
 
 
-<body>
-        <div>
-            <p class="MHMG">MHMG</p>
-        </div>
-        <div>
-            <p>Marcus Hornby Memory Game</p>
-        </div>
-       
-</body>
+// Color and Sound Functions
+  function sound(color) {
+    switch (color) {
+      case "#green":
+        game.sound.green.play();
+        break;
+      case "#red":
+        game.sound.red.play();
+        break;
+      case "#yellow":
+        game.sound.yellow.play();
+        break;
+      case "#blue":
+        game.sound.blue.play();
+        break;
+    }
+  }
+  
+  
+    // Start the game button function
+  $("#start").on("click",function() {
+    difficultySelect();
+    resetGame();
+    $("#count-num").html("--");
+    generateNext();
+  });
 
-</html>
+  // Reset the game function
+  function resetGame() {
+    game.completePattern = [];
+    game.randomisePattern = [];
+    game.count = 0;
+  }
+  
+    // difficulty selector
+  function difficultySelect() {
+    if ($("#difficult").is(":checked")){
+      game.difficult = true;
+    } else {
+      game.difficult = false;
+    }
+  }
+
+  function animation(color) {
+    $(color).addClass("animate");
+    sound(color);
+    setTimeout(function() {
+      $(color).removeClass("animate");
+    }, 250)
+  }
+
+
+  // add next random color
+  function generateNext() {
+    game.count++;
+    if (game.count < 10) {
+      $("#count-num").html("0" + game.count);
+    } else {
+      $("#count-num").html(game.count);
+    }
+    game.completePattern.push(game.colors[(Math.floor(Math.random() * 4))]);
+    randomisePattern();
+  }
+
+  // Play the entire sequence of colors
+  function randomisePattern() {
+    var i = 0;
+    var seq = setInterval(function() {
+      animation(game.completePattern[i]);
+      i++;
+      if (i >= game.completePattern.length) {
+        clearInterval(seq);
+      }
+    }, 700)
+    game.randomisePattern = [];
+  }
+
+  // Add whichever color the player clicked on onto the player's sequence
+  function rememberAndAddToSequence(id) {
+    var color = "#" + id;
+    console.log(color);
+    game.randomisePattern.push(color);
+    wasTheMoveCorrect(color);
+  }
+
+  // Check if the player move matched the last color
+  // If no, either re-play the sequence (normal mode) or end the game (strict mode)
+  // If yes, move on
+  // When count equals 20, the player wins!
+  function wasTheMoveCorrect(color) {
+    if (game.randomisePattern[game.randomisePattern.length - 1] !== game.completePattern[game.randomisePattern.length - 1]) {
+      sound(color);
+      if (game.difficult === true) {
+        $("#msg").text("You lost!");
+        $("#board").fadeOut("slow");
+        $("#final-screen").fadeTo("slow",1);
+      } else {
+        //warning message -------------------!!!!!!!!!!!!!!!
+        setTimeout(function() {
+            alert("Wrong move! Try again.");
+          }, 500);
+        setTimeout(function() {
+            randomisePattern();
+          }, 250);
+      }
+    } else {
+      sound(color);
+      var checkCorrect = game.randomisePattern.length === game.completePattern.length;
+      if (checkCorrect) {
+        if (game.count === 20) {
+          $("#msg").text("You won!");
+          $("#board").fadeOut("slow");
+          $("#completeGame").fadeTo("slow",1);
+        } else {
+          setTimeout(function() {
+            generateNext();
+          }, 850)
+        }
+      }
+    }
+  }
+
+ 
